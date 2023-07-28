5d7036c701e1b3127a111e9f86125ced282daa51
Kevin Robles <kevinronu@gmail.com>
2023-07-28 12:39:07 -0500

    chore: add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_one_attached :avatar
+
+  has_many :likes, dependent: :destroy
+  has_many :tweets, dependent: :destroy
+
+  VALID_EMAIL_REGEX = /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/i
+
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: VALID_EMAIL_REGEX, allow_blank: true }
+  validates :username, presence: true, uniqueness: true
+  validates :name, presence: true
+
+  enum role: { member: 0, admin: 1 }
 end
