5a2f224dad1ab86a98346b7b8ad16f019eb2ac87
Kevin Robles <kevinronu@gmail.com>
2023-07-28 12:30:29 -0500

    chore: update migration

--- db/migrate/20230728172858_create_tweets.rb
+++ db/migrate/20230728172858_create_tweets.rb
@@ -2,10 +2,10 @@ class CreateTweets < ActiveRecord::Migration[7.0]
   def change
     create_table :tweets do |t|
       t.text :body
-      t.integer :replies_count
-      t.integer :likes_count
+      t.integer :replies_count, default: 0
+      t.integer :likes_count, default: 0
       t.references :user, null: false, foreign_key: true
-      t.references :replied_to, null: false, foreign_key: true
+      t.references :replied_to, null: true, foreign_key: { to_table: :tweets }
 
       t.timestamps
     end
