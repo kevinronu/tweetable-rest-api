dcd142d26e70b4f19ffd328131658314a5c5365a
Kevin Robles <kevinronu@gmail.com>
2023-07-28 12:39:23 -0500

    chore: add like model validations

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
-  belongs_to :tweet
+  belongs_to :tweet, counter_cache: true
+
+  validates :user, uniqueness: { scope: :tweet, message: "and Tweet combination already taken" }
 end
