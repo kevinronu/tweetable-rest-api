0e83a8e67d117378c5e361a727e343b2425f2b9a
Kevin Robles <kevinronu@gmail.com>
2023-07-28 17:21:16 -0500

    chore: update likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,36 @@
 class LikesController < ApplicationController
-  def show; end
-  def create; end
+  before_action :set_like, only: %i[destroy]
 
-  def destroy; end
+  def show
+    @user = User.find(params[:id])
+    @likes = @user.likes.order(created_at: :desc)
+  end
+
+  def create
+    # binding.pry
+    @like = Like.new(like_params)
+
+    if @like.save
+      redirect_to fallback_location: root_path, notice: "Like test was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @like.destroy
+    redirect_to fallback_location: root_path, notice: "Like was successfully destroyed."
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_like
+    @like = Like.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def like_params
+    params.require(:like).permit(:user_id, :tweet_id)
+  end
 end
