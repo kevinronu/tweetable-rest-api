af657499c958edae706cdfdbca8f3ee8303c0329
Kevin Robles <kevinronu@gmail.com>
2023-07-29 12:40:20 -0500

    chore: update tweets_controller

--- app/controllers/api/tweets_controller.rb
+++ app/controllers/api/tweets_controller.rb
@@ -1,15 +1,33 @@
 module Api
   class TweetsController < ApiController
+    before_action :require_login!, except: %i[index show]
+
     # GET /api/tweets
     def index
       tweets = Tweet.all
-      render json: tweets, status: :ok
+      tweets_data = []
+      tweets.each do |tweet|
+        user = tweet.user
+        tweet_data = tweet.as_json(
+          except: %i[user_id]
+        ).merge(
+          username: user.username
+        )
+        tweets_data.push(tweet_data)
+      end
+      render json: tweets_data, status: :ok
     end
 
     # GET /api/tweets/:id
     def show
       tweet = Tweet.find(params[:id])
-      render json: tweet, status: :ok
+      user = tweet.user
+      tweet_data = tweet.as_json(
+        except: %i[user_id]
+      ).merge(
+        username: user.username
+      )
+      render json: tweet_data, status: :ok
     end
   end
 end
